4e4a4aaaef971cfffb4f1f586b241168dac50613
Cecil Phillip <cecilphillip@gmail.com>
2015-01-21 02:34:02 -0500

    playing with config sample

---
+++ src/ConfigurationSources/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.ConfigurationModel;
+
+namespace ConfigurationOptions
+{
+    public class Startup
+    {
+		public Startup()
+		{
+			Configuration = new Configuration()
+						 .AddJsonFile("config.json");
+        }
+		public IConfiguration Configuration { get; set; }
+		public void Configure(IApplicationBuilder app)
+        {
+			var setting = Configuration.Get("setting");
+			var number = Configuration.Get("number");
+			var upperWrong = Configuration.Get("upper");
+		  	var supper = Configuration.GetSubKey("upper")
+						.GetSubKey("nested")
+						.Get("innerValue");
+			
+
+
+        }
+    }	
+}
