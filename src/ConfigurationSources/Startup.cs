3cec16bc7ae8963b35103e9c639dbb4b8d26beab
Cecil Phillip <cecilphillip@gmail.com>
2015-01-24 14:19:26 -0500

    add taghelpers sample skeleton

--- src/ConfigurationSources/Startup.cs
+++ src/ConfigurationSources/Startup.cs
@@ -10,20 +10,22 @@ namespace ConfigurationOptions
 		public Startup()
 		{
 			Configuration = new Configuration()
-						 .AddJsonFile("config.json");
+						 .AddJsonFile("config.json")
+						 .AddIniFile("config.ini");
         }
 		public IConfiguration Configuration { get; set; }
 		public void Configure(IApplicationBuilder app)
         {
+			//from config.json
 			var setting = Configuration.Get("setting");
-			var number = Configuration.Get("number");
-			var upperWrong = Configuration.Get("upper");
+			var number = Configuration.Get("number");			
 		  	var supper = Configuration.GetSubKey("upper")
 						.GetSubKey("nested")
 						.Get("innerValue");
-			
 
-
+			//from config.ini
+			var connectionString = Configuration.Get("DefaultConnection:ConnectionString");
+			var provider = Configuration.Get("Data:Inventory:SubHeader:Provider");
         }
     }	
 }
---
+++ src/TagHelpersMvc/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Routing;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+
+namespace TagHelpersMvc
+{
+	public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime.
+        public void ConfigureServices(IServiceCollection services)
+        {           
+            services.AddMvc();
+        }
+       
+        public void Configure(IApplicationBuilder app)
+        {                    
+            // Add static files to the request pipeline.
+            app.UseStaticFiles();
+                    
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}",
+                    defaults: new { controller = "Home", action = "Index" });             
+            });
+        }
+    }
+}
