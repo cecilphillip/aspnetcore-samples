269da322ca26519b4da80402027b2021b63bacd1
Cecil Phillip <cecilphillip@gmail.com>
2015-01-28 21:38:30 +0000

    tagHelper sample

--- src/TagHelpersMvc/Controllers/HomeController.cs
+++ src/TagHelpersMvc/Controllers/HomeController.cs
@@ -1,35 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using Microsoft.AspNet.Mvc;
 using TagHelpersMvc.Models;
+using System;
 
 namespace TagHelpersMvc.Controllers
 {
     public class HomeController : Controller
     {
+        private static List<Contact> DataList { get; set; }
+        static HomeController()
+        {
+            DataList = new List<Contact> {
+                new Contact { Id="A-101", Name = "Cecil", PhoneNumber = "555-000-5555" },
+                new Contact { Id="A-102", Name = "Dave", PhoneNumber = "555-111-5555" },
+                new Contact { Id="A-103", Name = "Richie", PhoneNumber = "555-222-5555" },
+                new Contact { Id="A-104", Name = "Janier", PhoneNumber = "555-333-5555" },
+            };
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-		[HttpPost]
-		public IActionResult Index([Bind("Name", "DateOfBirth", "PhoneNumber")] Contact model)
-		{
-			return View();
-		}
 
-		public IActionResult About()
+        [HttpPost]
+        public IActionResult Index([Bind("Name", "DateOfBirth", "PhoneNumber")] Contact model)
         {
-            ViewBag.Message = "Your application description page.";
-
+            model.Id =  "A-" + (new Random()).Next(105, 140);
+            DataList.Add(model);
             return View();
         }
 
-        public IActionResult Contact()
+        public IActionResult List()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your application description page.";
 
-            return View();
+            return View(DataList);
         }
 
         public IActionResult Error()
---
+++ src/TagHelpersMvc/Helpers/ContactListTagHelper.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+using Microsoft.AspNet.Razor.TagHelpers;
+using System.Linq;
+using System.Collections.Generic;
+using TagHelpersMvc.Models;
+
+namespace TagHelpersMvc.Helpers
+{    
+    [ContentBehavior(ContentBehavior.Replace)]
+    [TagName("contactlist")]
+    public class ContactListTagHelper : TagHelper
+    {
+        [Activate]
+        protected internal ViewContext ViewContext { get; set; }
+
+        [HtmlAttributeName("model")]
+        public IEnumerable<Contact> For { get; set; }
+
+        #region Templates
+        private string template_header = @"
+            <div class=""panel panel-default"">
+                <div class=""panel-heading"">
+                    <span class=""title"">Contacts</span>
+                    <ul class=""pull-right c-list"">
+                        <li>
+                            <a><i class=""glyphicon glyphicon-plus""></i></a>
+                      </li>
+                    </ul>
+                </div>
+                <div class=""panel-body"">
+                    <ul class=""list-group"">  ";
+
+        private string template_body = @"
+            <li class=""list-group-item"">
+                <div class=""col-xs-12 col-sm-9"">
+                    <span>{0}</span><br />
+                    <span>{1}</span><br />
+                    <span>{2}</span>
+                </div>
+                <div class=""clearfix""></div>
+            </li>";
+
+        private string template_footer = @"</ul></div></div>";
+        #endregion
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.SelfClosing = false;
+
+            if (For == null || !For.Any()) return;
+            output.Content = template_header;
+            For.ToList().ForEach(c =>
+            {
+                output.Content += GenerateContainer(c);
+            });
+
+            output.Content += template_footer;
+        }
+
+        private string GenerateContainer(Contact model)
+        {
+            return string.Format(template_body, model.Name, model.PhoneNumber, model.DateOfBirth.ToString("MMM dd yyy"));
+        }
+    }
+}
