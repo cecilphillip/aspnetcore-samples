41cd721ef57b26be3b81e53ab228227745bdd95f
Cecil Phillip <cecilphillip@gmail.com>
2015-02-04 20:56:02 +0000

    model binding contact controller

---
+++ src/MobelBindingMvc/Controllers/ContactController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using MobelBindingMvc.Models;
+
+namespace MobelBindingMvc.Controllers
+{    
+    public class FriendController : Controller
+    {
+        [HttpPost]
+        public IActionResult Name([FromForm]string name) {
+            return new HttpStatusCodeResult(200);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]Friend model)
+        {            
+            return new HttpStatusCodeResult(201);
+        }
+
+        [HttpPost]
+        public IActionResult Bunch([FromBody]List<Friend> model)
+        {
+            return new HttpStatusCodeResult(201);
+        }
+    }
+}
--- src/MobelBindingMvc/Startup.cs
+++ src/MobelBindingMvc/Startup.cs
@@ -1,45 +1,27 @@
 ﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Diagnostics;
-using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Routing;
-using Microsoft.Framework.ConfigurationModel;
 using Microsoft.Framework.DependencyInjection;
 
 namespace MobelBindingMvc
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
-        {
-            // Setup configuration sources.
-            Configuration = new Configuration()
-                .AddJsonFile("config.json")
-                .AddEnvironmentVariables();
-        }
-
-        public IConfiguration Configuration { get; set; }
-
-        // This method gets called by the runtime.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
         }
 
-        // Configure is called after ConfigureServices is called.
         public void Configure(IApplicationBuilder app)
         {
             app.UseErrorPage(ErrorPageOptions.ShowAll);
-
-            // Add static files to the request pipeline.
-            app.UseStaticFiles();
             
-            // Add MVC to the request pipeline.
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller}/{action}",
-                    defaults: new { controller = "Home", action = "Index" });               
+                    defaults: new { controller = "Home", action = "Index" });
             });
         }
     }
