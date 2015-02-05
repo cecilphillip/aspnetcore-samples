1d1ad7ac3bcdde358a1573a697d63584b1f9c460
Cecil Phillip <cecilphillip@gmail.com>
2015-02-04 22:36:25 -0500

    clean up

--- src/WebApiShimMvc/Startup.cs
+++ src/WebApiShimMvc/Startup.cs
@@ -11,7 +11,7 @@ namespace WebApiShimMvc
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup()
         {
             // Setup configuration sources.
             Configuration = new Configuration()
@@ -24,6 +24,7 @@ namespace WebApiShimMvc
         // Configure is called after ConfigureServices is called.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerfactory)
         {
+			loggerfactory.AddConsole();
             app.UseErrorPage(ErrorPageOptions.ShowAll);
 
             app.UseServices(services =>
