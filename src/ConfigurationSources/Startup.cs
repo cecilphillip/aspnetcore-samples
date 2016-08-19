f6abb4dca499d94335fc04cdbb70c2988f5f2a04
Cecil Phillip <cecilphillip@gmail.com>
2016-08-19 16:44:25 -0400

    upgrade ConfigurationSources demo to RTM

--- src/ConfigurationSources/Startup.cs
+++ src/ConfigurationSources/Startup.cs
@@ -1,29 +1,34 @@
-﻿using Microsoft.AspNet.Builder;
-using Microsoft.Framework.ConfigurationModel;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace ConfigurationOptions
 {
     public class Startup
     {
-		public Startup()
-		{
-			Configuration = new Configuration()
-						 .AddJsonFile("config.json")
-						 .AddIniFile("config.ini");
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddIniFile("config.ini")
+                .AddJsonFile("config.json");
+
+            Configuration = builder.Build();
         }
+
 		public IConfiguration Configuration { get; set; }
-		public void Configure(IApplicationBuilder app)
+
+        public void Configure(IApplicationBuilder app)
         {
 			//from config.json
-			var setting = Configuration.Get("setting");
-			var number = Configuration.Get("number");			
-		  	var supper = Configuration.GetSubKey("upper")
-						.GetSubKey("nested")
-						.Get("innerValue");
+			var setting = Configuration["setting"];
+			var number = Configuration["number"];			
+		  	var supper = Configuration.GetSection("upper")
+						.GetSection("nested")["innerValue"];
 
 			//from config.ini
-			var connectionString = Configuration.Get("DefaultConnection:ConnectionString");
-			var provider = Configuration.Get("Data:Inventory:SubHeader:Provider");
+			var connectionString = Configuration["DefaultConnection:ConnectionString"];
+			var provider = Configuration["Data:Inventory:SubHeader:Provider"];
         }
     }	
 }
